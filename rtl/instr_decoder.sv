297dd61ed5782af2f73a3d3a2af894c571d81e1e
TheMadMike <m.gibas2407@gmail.com>
2023-05-15 22:59:45 +0200

    Change instruction decoder module from synchronous block to fully combitional

--- rtl/instr_decoder.sv
+++ rtl/instr_decoder.sv
@@ -5,29 +5,34 @@ module instr_decoder
     input clk,
     input ce,
     input instr_t instr,
-    output decoder_ctl_signals_t ctl_signals
+    output decoder_ctl_signals_t ctl_signals,
+    output [4:0] rd_idx,
+    output [4:0] rs1_idx,
+    output [4:0] rs2_idx,
+    output [31:0] rd_in
 );
-    instr_t instr_reg;
-
+    // synthesis translate_off
     always @ (posedge clk)
     begin
         if (ce)
         begin
-            // synthesis translate_off
-            $display("\t [DECODE]");
-            $display("Decoding instruction: \t0x%8h", instr.word);
-            $display("Opcode: \t\t0b%7b [%s]", instr.any.opcode, instr.any.opcode.name());
+            $display("\tDecoding instruction: \t0x%8h", instr.word);
+            $display("\tOpcode: \t\t0b%7b [%s]", instr.any.opcode, instr.any.opcode.name());
 
             if (instr.any.opcode != LUI)
             begin
                 $display("instruction not supported!");
             end 
-            // synthesis translate_on
-            
-            instr_reg <= instr;
         end 
     end
+    // synthesis translate_on 
 
-    assign ctl_signals.regfile_we = (instr_reg.any.opcode == LUI) ? 1 : 0;
+    assign ctl_signals.regfile_we = (instr.any.opcode == LUI) ? 1 : 0;
+
+    assign rs1_idx = 5'b0;
+    assign rs2_idx = 5'b0;
+
+    assign rd_idx = (instr.any.opcode == LUI) ? instr.utype.rd : 5'b0; 
+    assign rd_in = (instr.any.opcode == LUI) ? { instr.utype.imm, 12'b0 } : 32'b0;
 
 endmodule
