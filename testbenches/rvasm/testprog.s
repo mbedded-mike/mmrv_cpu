a875c1dfd6b896cf396f398f9f777e352f815609
mbedded-mike <m.gibas2407@gmail.com>
2023-06-08 20:54:21 +0200

    Add some explanatory comments to testprog.s

--- testbenches/rvasm/testprog.s
+++ testbenches/rvasm/testprog.s
@@ -1,4 +1,4 @@
 lui t2, 1
 auipc t1, 1
-jal ra, 8
-auipc t3, 0xc0c
+jal ra, 8       # jump back to itself and link (ifinite loop)
+auipc t3, 0xc0c # this should never execute because of the previous jump
